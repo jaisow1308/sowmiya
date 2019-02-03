0b4cc771f98b15ba2f5ff1978caab25845f582e2
jaisow1308 <47234264+jaisow1308@users.noreply.github.com>
2019-02-03 08:48:30 -0800

    Create vove.py

---
+++ vove.py
@@ -0,0 +1,6 @@
+ch=raw_input()
+if (ch=='a'or ch=='e'or ch=='i'or ch=='o'or ch=='u'or ch=='A'or ch=='E'or ch=='I'or ch=='I'or ch=='O'or ch=='U'):print 'Vowel'
+elif ch=='$':
+print 'invalid'
+else:
+	print 'Consonant'
